bc1891b1072d3dacb030d33a9ef18201b72cacbb
AlexCaoGZ <82902418+AlexCaoGZ@users.noreply.github.com>
2021-11-27 21:26:31 -0500

    PlanerTasksTest.cs added

---
+++ UnitTests/PlannerTasksTest.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SQ_TMS_Project;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class PlannerTasksTest
+    {
+        [TestMethod]
+        public void IsReviewCustomersTrue()
+        {
+            BuyerTasks planTest = new BuyerTasks();
+            Assert.AreEqual(true, planTest.ReceiversOrder(0,0));
+        }
+
+        [TestMethod]
+        public void IsReviewCustomersFalse()
+        {
+            PlannerTasks planTest = new PlannerTasks();
+            Assert.AreEqual(false, planTest.SelectCarriers(0,50));
+        }
+
+        [TestMethod]
+        public void newOrderTrue()
+        {
+            PlannerTasks planTest = new PlannerTasks();
+            Assert.AreEqual(true, planTest.WhenSplitToMultipleTrips(0,50));
+        }
+
+        [TestMethod]
+        public void newOrderFalse()
+        {
+            PlannerTasks planTest = new PlannerTasks();
+            Assert.AreEqual(false, planTest.OneDayIncrements(0));
+        }
+
+        [TestMethod]
+        public void isCityTrue()
+        {
+            PlannerTasks planTest = new PlannerTasks();
+            Assert.AreEqual(true, planTest.ConfirmOrderCompleted(0));
+        }
+
+        [TestMethod]
+        public void IsCityFalse()
+        {
+            PlannerTasks planTest = new PlannerTasks();
+            Assert.AreEqual(false, planTest.PrintSummary(0));
+        }
+
+
+        [TestMethod]
+        public void isInvoiceGenerationTrue()
+        {
+            PlannerTasks planTest = new PlannerTasks();
+            Assert.AreEqual(true, planTest.PrintSummary(1));
+        }
+
+        [TestMethod]
+        public void isInvoiceGenerationFalse()
+        {
+            PlannerTasks planTest = new PlannerTasks();
+            Assert.AreEqual(false, planTest.PrintSummary(2));
+        }
+    }
+}
